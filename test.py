52a27b06e88b7602d5582d367cc4d0819d5d4782
okonu <d.okonu1@gmail.com>
2021-11-23 01:03:59 -0600

    auto - cases sum

--- test.py
+++ test.py
@@ -17,3 +17,9 @@ try:
         time.sleep(0.05)
 except:
     GPIO.cleanup() 
+
+# seamer 8
+# counter1 = 37
+# GPIO.setup(counter1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# counter2 = 33
+# GPIO.setup(counter2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
