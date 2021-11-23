45b313391d0056f9fa90180820dc3720c5695d57
okonu <d.okonu1@gmail.com>
2021-11-23 00:53:33 -0600

    auto - cases sum

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ GPIO.setmode(GPIO.BOARD)
 
 counter1 = 37
 
-# GPIO.setup(counter1, GPIO.IN)
-GPIO.setup(counter1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(counter1, GPIO.IN)
+# GPIO.setup(counter1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 try:  
