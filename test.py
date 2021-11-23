0b63a7c3be41545e5995b6db4a3361dd63037b0d
okonu <d.okonu1@gmail.com>
2021-11-23 00:52:32 -0600

    auto - cases sum

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ GPIO.setmode(GPIO.BOARD)
 counter1 = 37
 
 # GPIO.setup(counter1, GPIO.IN)
-GPIO.setup(counter1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(counter1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 try:  
     while True : 
         print (GPIO.input(counter1)) 
-        time.sleep(0.01)
+        time.sleep(0.05)
 except:
     GPIO.cleanup() 
