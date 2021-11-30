12186358a1df56dd7ccbcfd321ab54300c83e16e
okonu <d.okonu1@gmail.com>
2021-11-29 23:35:31 -0600

    updated new payloade

--- app.py
+++ app.py
@@ -107,8 +107,8 @@ def connect_mqtt():
 client = connect_mqtt()
 
 def publish():
-    global cnt1, cnt2, cont2, downtime
-    msg = '{"clientID":"'+ str(client_id) +'","cans":" ' + str(cnt2) + '","cases":"' + str(round(cnt2/canspercase,1)) + '","cspeed":"'+ str(cspeed * 60) +'","tstamp":"1385816","downtime":"'+str(downtime)+'"}'
+    global cnt1, cnt2, cont2, downtime, damages
+    msg = '{"clientID":"'+ str(client_id) +'","cans":" ' + str(cnt2) + '","cases":"' + str(round(cnt2/canspercase,1)) + '","cspeed":"'+ str(cspeed * 60) +'","tstamp":"'+str(time.time()) +'","damages":"'+str(damages)+'","downtime":"'+str(downtime)+'"}'
     topic = 'test'
     result = client.publish(topic, msg)
     status = result[0]
