5b20bb87d876aea76a45808befb3d52c41398049
okonu <d.okonu1@gmail.com>
2021-12-05 03:55:45 -0600

    testing interrupt

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ sudo rm -rf /home/pi/.config/autostart &&
 mkdir /home/pi/.config/autostart &&
 sleep 3 &&
 echo 'creating the startup.sh file at /home/pi' &&
-echo $'kill -9 $(lsof -t -i:27011) || true\nNOW=$(date +"log_%Y-%m-%d_%H:%M:%S.txt")\ncd /home/pi/project/l_screens && python3 app.py > /home/pi/Desktop/$NOW 2>&1' > /home/pi/startup.sh &&
+echo $'kill -9 $(lsof -t -i:27011) || true\nNOW=$(date +"log_%Y-%m-%d_%H:%M:%S.txt")\ncd /home/pi/project/HMI2 && python3 app.py > /home/pi/Desktop/$NOW 2>&1' > /home/pi/startup.sh &&
 chmod 755 /home/pi/startup.sh &&
 
 sleep 3 &&
